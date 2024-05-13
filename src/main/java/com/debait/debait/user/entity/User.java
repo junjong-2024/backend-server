b0e4063a833392f9b0f504cf4acb8fd97e346bac
seyxxn <lucy9480@gmail.com>
2024-05-13 14:46:13 +0900

    login 관련 에러 발생 코드 제외

--- src/main/java/com/debait/debait/user/entity/User.java
+++ src/main/java/com/debait/debait/user/entity/User.java
@@ -3,13 +3,8 @@ package com.debait.debait.user.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 @Entity
 @Table(name = "users")
 @Getter

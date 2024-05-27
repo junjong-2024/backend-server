c488165852b1f94261f42d04b320aa0558731004
seyxxn <lucy9480@gmail.com>
2024-05-27 15:00:08 +0900

    feat: CorsConfig 파일 추가

---
+++ src/main/java/com/debait/debait/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.debait.debait.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true) // 쿠키 허용 여부
+                .maxAge(3600); // preflight 요청의 캐시 시간
+    }
+}

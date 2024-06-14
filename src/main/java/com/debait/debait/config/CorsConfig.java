c5444478fc071395e0a5278c481656898c061e72
seyxxn <lucy9480@gmail.com>
2024-06-14 16:24:52 +0900

    refact : CorsConfig.java 수정

--- src/main/java/com/debait/debait/config/CorsConfig.java
+++ src/main/java/com/debait/debait/config/CorsConfig.java
@@ -9,10 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(true) // 쿠키 허용 여부
-                .maxAge(3600); // preflight 요청의 캐시 시간
+                .allowedOrigins("*"); // 허용할 도메인
     }
 }
